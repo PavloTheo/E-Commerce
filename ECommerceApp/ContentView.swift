f49ce608ae515e58ab2981a8c7a3e252f2afc4f4
Your GitHub UsernamePavloTheo git config --global user.email your-email@example.compavlo.theodoridis@gmail.com <pavlo.theodoridis@gmail.com>
2024-05-22 10:19:04 +0200

    Initial Commit

---
+++ ECommerceApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ECommerceApp
+//
+//  Created by Pavlo Theodoridis on 2024-05-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ECommerceApp/ECommerceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECommerceAppApp.swift
+//  ECommerceApp
+//
+//  Created by Pavlo Theodoridis on 2024-05-22.
+//
+
+import SwiftUI
+
+@main
+struct ECommerceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
